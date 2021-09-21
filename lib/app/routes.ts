ca1bff4ab78a81fdfb86ab76f5247857930e6103
Kane Baccigalupi <baccigalupi@gmail.com>
2021-09-20 21:11:10 -0700

    Changes routes.js to routes.ts
    * Rewrites simpler with just some classes
    * Changes interface some :shrug:

---
+++ lib/app/routes.ts
@@ -0,0 +1,77 @@
+// deno-lint-ignore-file no-explicit-any
+// TODO: addd Component or Template type where any exists now
+type Component = any;
+
+interface MatchableRoute {
+  match(path: string): boolean
+}
+
+class ExactRoute implements MatchableRoute {
+  path: string;
+  component: Component;
+
+  constructor(path: string, component: Component) {
+    this.path = path;
+    this.component = component;
+  }
+
+  match(path: string): boolean {
+    return this.path.trim() === path.trim()
+  }
+}
+
+class FuzzyRoute implements MatchableRoute {
+  matcher: RegExp;
+  component: Component;
+
+  constructor(matcher: RegExp, component: Component) {
+    this.matcher = matcher;
+    this.component = component;
+  }
+
+  match(path: string): boolean {
+    return this.matcher.test(path.trim());
+  }
+
+  matches(path: string): Array<string> {
+    return path.trim().match(this.matcher) || [];
+  }
+}
+
+type Route = ExactRoute | FuzzyRoute;
+type RouteCollection = Array<Route>;
+
+export class Routes {
+  collection: RouteCollection;
+  defaultRoute: Component | undefined;
+
+  constructor() {
+    this.collection = [];
+  }
+
+  addPath(path: string, component: Component): Routes {
+    this.collection.push(new ExactRoute(path, component));
+    return this;
+  }
+
+  addMatcher(matcher: RegExp, component: Component): Routes {
+    this.collection.push(new FuzzyRoute(matcher, component));
+    return this;
+  }
+
+  addDefault(component: Component): Routes {
+    this.defaultRoute = new FuzzyRoute(/.*/, component);
+    return this;
+  }
+
+  getRoute(path: string): Route | undefined {
+    const matched = this.collection.find((route: Route) => {
+      return route.match(path);
+    });
+    
+    return matched || this.defaultRoute;
+  }
+}
+
+const routes = () => new Routes();
+export default routes;
--- test/app/routes.test.jsx
+++ test/app/routes.test.jsx
@@ -1,7 +1,7 @@
 import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
 
 import jsx from '../../lib/jsx.js';
-import routes from '../../lib/app/routes.js';
+import routes from '../../lib/app/routes.ts';
 
 Deno.test('Routes: Routes: starts as empty', () => {
   const pages = routes();
@@ -10,7 +10,7 @@ Deno.test('Routes: Routes: starts as empty', () => {
 
 Deno.test('Routes: getting a route will return a default when no match and default is configured', () => {
   const NotFound = () => <h1>Nope, not here!</h1>;
-  const pages = routes().default(NotFound);
+  const pages = routes().addDefault(NotFound);
   assertEquals(pages.getRoute('/').component, NotFound);
   assertEquals(pages.getRoute('/ueaumr-chaeud').component, NotFound);
 });
@@ -29,20 +29,20 @@ Deno.test('Routes: finds routes by regex, and provides matcher information', () 
 
   const pages = routes()
     .addMatcher(/^\/(hello|hi)(.*)/, Hello)
-    .default(NotFound);
+    .addDefault(NotFound);
 
   assertEquals(pages.getRoute('/hello').component, Hello);
   assertEquals(pages.getRoute('/hi').component, Hello);
 
   let route = pages.getRoute('/hellow');
   assertEquals(route.component, Hello);
-  assertEquals(route.matches[1], 'hello');
-  assertEquals(route.matches[2], 'w');
+  assertEquals(route.matches('/hellow')[1], 'hello');
+  assertEquals(route.matches('/hellow')[2], 'w');
 
   route = pages.getRoute('/hiya');
   assertEquals(route.component, Hello);
-  assertEquals(route.matches[1], 'hi');
-  assertEquals(route.matches[2], 'ya');
+  assertEquals(route.matches('/hiya')[1], 'hi');
+  assertEquals(route.matches('/hiya')[2], 'ya');
 });
 
 Deno.test('Routes: returns the first match', () => {
@@ -69,7 +69,7 @@ Deno.test('Routes: uses the default only if there are not other matches', () => 
   const NotFound = () => <h1>Nope, not here!</h1>;
 
   const pages = routes()
-    .default(NotFound)
+    .addDefault(NotFound)
     .addMatcher(/^\/(hello|hi)(.*)/, Hello);
 
   assertEquals(pages.getRoute('/hi').component, Hello);
