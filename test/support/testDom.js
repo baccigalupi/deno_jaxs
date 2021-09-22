d72dc5f35ec62c0803838ed6f97f0f0639ffad00
Kane Baccigalupi <baccigalupi@gmail.com>
2021-09-21 18:34:26 -0700

    Fixes bin/test script to take an argument

--- test/support/testDom.js
+++ test/support/testDom.js
@@ -1,10 +1,16 @@
-import { DOMParser, Element } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+import {
+  DOMParser,
+  Element,
+} from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts';
 import { isArray } from '../../lib/utilities/array.js';
 
 const defaultContent = '<div id=\'app\'></div>';
 
 export const createTestDom = (content = defaultContent) => {
-  return new DOMParser().parseFromString(`<!DOCTYPE html><body>${content}<body>`, "text/html");
+  return new DOMParser().parseFromString(
+    `<!DOCTYPE html><body>${content}<body>`,
+    'text/html',
+  );
 };
 
 export const domToString = (element) => {
