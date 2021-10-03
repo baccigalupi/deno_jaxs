26aad0ff9c7e019eb4c4227a8d74e2b81a9b4ec5
Kane Baccigalupi <baccigalupi@gmail.com>
2021-10-03 10:44:02 -0700

    Fixes the utilities tests to new lib

--- test/utilities/array.test.js
+++ test/utilities/array.test.js
@@ -1,27 +1,39 @@
+import { testSuite } from 'https://raw.githubusercontent.com/baccigalupi/deno_describeIt/main/lib/testSuite.ts';
+
 import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
 import { ensureArray, isArray } from '../../lib/utilities/array.ts';
 
-Deno.test('Utilities, array: isArray will check whether its an array', () => {
-  assertEquals(isArray(''), false);
-  assertEquals(isArray({}), false);
-  assertEquals(isArray(undefined), false);
-  assertEquals(isArray(null), false);
-  assertEquals(isArray([]), true);
-  assertEquals(isArray(['hello']), true);
+const { describe, it, run } = testSuite();
+
+describe('Utilities', () => {
+  describe('array', () => {
+    it('isArray will check whether its an array', () => {
+      assertEquals(isArray(''), false);
+      assertEquals(isArray({}), false);
+      assertEquals(isArray(undefined), false);
+      assertEquals(isArray(null), false);
+      assertEquals(isArray([]), true);
+      assertEquals(isArray(['hello']), true);
+    });
+
+    describe('ensureArray', () => {
+      it('converts values to array with that element', () => {
+        assertEquals(ensureArray('foo'), ['foo']);
+        assertEquals(ensureArray({}), [{}]);
+      });
+
+      it('turns falsey values into empty arrays', () => {
+        assertEquals(ensureArray(''), []);
+        assertEquals(ensureArray(null), []);
+        assertEquals(ensureArray(undefined), []);
+      });
+
+      it('leaves arrays as they are', () => {
+        assertEquals(ensureArray([]), []);
+        assertEquals(ensureArray(['foo']), ['foo']);
+      });
+    });
+  });
 });
 
-Deno.test('Utilities, array: ensureArray converts values to array with that element', () => {
-  assertEquals(ensureArray('foo'), ['foo']);
-  assertEquals(ensureArray({}), [{}]);
-});
-
-Deno.test('Utilities, array: ensureArray turns falsey values into empty arrays', () => {
-  assertEquals(ensureArray(''), []);
-  assertEquals(ensureArray(null), []);
-  assertEquals(ensureArray(undefined), []);
-});
-
-Deno.test('Utilities, array: leaves arrays as they are', () => {
-  assertEquals(ensureArray([]), []);
-  assertEquals(ensureArray(['foo']), ['foo']);
-});
+await run();
--- test/utilities/dom.test.js
+++ test/utilities/dom.test.js
@@ -1,33 +1,45 @@
+import { testSuite } from 'https://raw.githubusercontent.com/baccigalupi/deno_describeIt/main/lib/testSuite.ts';
 import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
 import { findHref, findTag } from '../../lib/utilities/dom.js';
-
 import { createTestDom } from '../support/testDom.js';
 
-Deno.test('DOM utilities: findHref finds the href when it is the direct target node', () => {
-  const document = createTestDom('<a id="find-me" href="/foo"></a>');
-  const node = document.getElementById('find-me');
-  assertEquals(findHref(node), '/foo');
+const { describe, it, run } = testSuite();
+
+describe('Utilities', () => {
+  describe('dom', () => {
+    describe('findHref', () => {
+      it('finds the href when it is the direct target node', () => {
+        const document = createTestDom('<a id="find-me" href="/foo"></a>');
+        const node = document.getElementById('find-me');
+        assertEquals(findHref(node), '/foo');
+      });
+
+      it('finds the href at the nearest parent when not available on target node', () => {
+        const document = createTestDom(
+          '<a id="find-me" href="/foo"><ul><li id="click-me">Click me</li></ul></a>',
+        );
+        const node = document.getElementById('click-me');
+        assertEquals(findHref(node), '/foo');
+      });
+    });
+
+    describe('findTag', () => {
+      it('finds the current node when it has a matching tag', () => {
+        const document = createTestDom('<a id="find-me" href="/foo"></a>');
+        const node = document.getElementById('find-me');
+        assertEquals(findTag('a', node), node);
+      });
+
+      it('finds nearest parent node when not the right tag name', () => {
+        const document = createTestDom(
+          '<a id="find-me" href="/foo"><ul><li id="click-me">Click me</li></ul></a>',
+        );
+        const child = document.getElementById('click-me');
+        const node = document.getElementById('find-me');
+        assertEquals(findTag('a', child), node);
+      });
+    });
+  });
 });
 
-Deno.test('DOM utilities: findHref finds the href at the nearest parent when not available on target node', () => {
-  const document = createTestDom(
-    '<a id="find-me" href="/foo"><ul><li id="click-me">Click me</li></ul></a>',
-  );
-  const node = document.getElementById('click-me');
-  assertEquals(findHref(node), '/foo');
-});
-
-Deno.test('DOM utilities: findTag finds the current node when it has a matching tag', () => {
-  const document = createTestDom('<a id="find-me" href="/foo"></a>');
-  const node = document.getElementById('find-me');
-  assertEquals(findTag('a', node), node);
-});
-
-Deno.test('DOM utilities: findTag finds nearest parent node when not the right tag name', () => {
-  const document = createTestDom(
-    '<a id="find-me" href="/foo"><ul><li id="click-me">Click me</li></ul></a>',
-  );
-  const child = document.getElementById('click-me');
-  const node = document.getElementById('find-me');
-  assertEquals(findTag('a', child), node);
-});
+await run();
