0422d02efc960bba86eeeea4ef41c0b6d0e0ba10
Kane Baccigalupi <baccigalupi@gmail.com>
2022-02-14 22:33:40 -0800

    Moving dependencies to a file for better orchestraction

---
+++ test/dependencies.js
@@ -0,0 +1,6 @@
+export { testSuite } from 'https://raw.githubusercontent.com/baccigalupi/deno_describeIt/main/lib/testSuite.ts';
+export * as asserts from 'https://deno.land/std/testing/asserts.ts';
+export * as spy from 'https://deno.land/x/mock@v0.10.0/spy.ts';
+export { listenAndServe } from 'https://deno.land/std/http/server.ts';
+export { DOMParser } from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts';
+
